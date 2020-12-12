baf0ac9d7b148188ed6d7723e17836d5f08a5f50
cswitzer <50593813+cswitzer@users.noreply.github.com>
2020-12-11 23:47:52 -0700

    Added rocket movement
    Coded thrusting and rotational (on Z axis) movement for the rocket

---
+++ Project Escape/Assets/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rocketRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the reference to the rocket ship's rigid body
+        rocketRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // GetKeyDown() is different. It only applies for jumping or one time firing
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // add force to the direction the rocket is facing
+            rocketRigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // rocket will rotate around the Z axis
+            transform.Rotate(Vector3.forward);
+            
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
