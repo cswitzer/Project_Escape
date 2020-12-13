0e79c867eec968d60d97e5d6d79e58cbc744195e
cswitzer <50593813+cswitzer@users.noreply.github.com>
2020-12-13 13:50:47 -0700

    Applied adjustable params
    Gave rocket serializable fields (params that can be accessed in unity editor but no outside the class)

--- Project Escape/Assets/Rocket.cs
+++ Project Escape/Assets/Rocket.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    // adjustable parameters
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    // rocket components
     Rigidbody rocketRigidBody;
     AudioSource rocketAudioSource;
 
@@ -24,11 +29,17 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
         // GetKeyDown() is different. It only applies for jumping or one time firing
         if (Input.GetKey(KeyCode.Space))
         {
             // add force to the direction the rocket is facing
-            rocketRigidBody.AddRelativeForce(Vector3.up);
+            rocketRigidBody.AddRelativeForce(Vector3.up * mainThrust);
             // do not layer the audio source
             if (!rocketAudioSource.isPlaying)
             {
@@ -39,16 +50,27 @@ public class Rocket : MonoBehaviour
         {
             rocketAudioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        // so the rocket doesn't spin out of control
+        rocketRigidBody.freezeRotation = true; // take manual control of rotation
+        
+        // make speed of rotation the same for all computers
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
-        {
+        {       
             // rocket will rotate around the Z axis
-            transform.Rotate(Vector3.forward);
-            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rocketRigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
